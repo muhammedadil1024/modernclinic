763806b01603732c1e392828d02b6c21c1eaabfb
muhammedadil1024 <muhammedadilpv292@gmail.com>
2025-02-24 07:50:49 +0530

    Refactor static file settings in settings

--- turtorial_yt/settings.py
+++ turtorial_yt/settings.py
@@ -126,17 +126,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
 if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
-    # and renames the files with unique names for each version to support long-term caching
+    STATIC_ROOT = BASE_DIR / 'staticfiles'
+    # WhiteNoise storage for compressing and caching static files
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
 MEDIA_ROOT = BASE_DIR / 'uploads'
 
 MEDIA_URL = '/media/'
